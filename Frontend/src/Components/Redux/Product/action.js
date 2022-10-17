f83a1de132c30b525a5d16f0350dae85d8a73bdf
jaiomkul <94021305+jaiomkul@users.noreply.github.com>
2022-10-17 11:26:03 +0530

    api

--- Frontend/src/Components/Redux/Product/action.js
+++ Frontend/src/Components/Redux/Product/action.js
@@ -24,7 +24,7 @@ const handelError = () => ({
 
 export const getData = () => (dispatch) => {
   dispatch(handelLoading());
-  fetch("http://localhost:8080/products")
+  fetch(`https://mini-store.herokuapp.com/products`)
     .then((res) => res.json())
     .then((res) => dispatch(handelSuccess(res.products)))
     .catch(() => dispatch(handelError()));
@@ -47,7 +47,7 @@ const handelSingleProductError = () => ({
 
 export const getSingleProductData = (id) => (dispatch) => {
   dispatch(handelSingleProductLoading());
-  fetch(`http://localhost:8080/products/${id}`)
+  fetch(`https://mini-store.herokuapp.com/products/${id}`)
     .then((res) => res.json())
     .then((res) => dispatch(handelSingleProductSuccess(res)))
     .catch(() => dispatch(handelSingleProductError()));
